c9c7b5d990754d407a7bda3baf41c5cd54cb0e6d
Peter K Chen <i@peterkchen.com>
2012-09-25 12:59:55 -0700

    recent updates

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, '380121918698790', '319f251fc875dde2c3f5fd44b7fbd8da'
   #provider :facebook, '380121918698790', '319f251fc875dde2c3f5fd44b7fbd8da', :scope => 'email,offline_access'#, :display => 'popup'
   provider :twitter, 'a8uYjTwOukqYxm9S97Xk1g', 'wtn6WvMp56C6glHzXJd2aEmC2zzG50PdMJAycdt1Xs'
-  provider :facebook, '146239612181043', '1f8dd73e2b62cdebec483bcfd2b7612e', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.development?
-  provider :facebook, '406898949368928', '6999bcaf6fe31bc63c85be6df690e320', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.production?
+  provider :facebook, '343588415732330', '8355ede143963a61b624d82ff354dc08', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.development?
+  provider :facebook, '524356277579122', '5284545fece303b49d41d1d627dcd99f', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.production?
 end
