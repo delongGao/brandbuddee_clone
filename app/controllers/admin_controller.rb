73586cb66da2434331868c64b4f8d23f4e9ccf77
Peter K Chen <pkc@Peters-MacBook-Pro.local>
2012-12-17 04:48:18 -0800

    new users admin stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,10 +6,13 @@ class AdminController < ApplicationController
 			if current_user.account_type == 'super admin' || Rails.env.development?
 				@share_month = Campaign.where(:date.gt => Time.now - 1.month)
 				@share_two_months = Campaign.where(:date.gt => Time.now - 2.month)
-
 				@share_month_count = @share_month.count
 				@share_last_month_count = @share_two_months.count - @share_month_count
 
+				@user_month = User.where(:date.gt => Time.now - 1.month)
+				@user_two_months = User.where(:date.gt => Time.now - 2.month)
+				@user_month_count = @user_month.count
+				@user_last_month_count = @user_two_months.count - @user_month_count
 				
 
 				#@user_this_month = User.where(:date.gt => Time.now - 1.month, :date.lte => Time.now)
