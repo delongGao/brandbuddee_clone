99532487b1192c3e1d23b51438e7cae6c81d32cb
Brice Bentler <server4001@gmail.com>
2013-05-08 13:31:19 -0700

    Change Account Ids For Amazon AWS, Facebook, And Twitter

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
     config.cache_dir = 'carrierwave' # ...two lines    
     config.s3_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.s3_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.s3_bucket = 'brandbuddee'
+    config.s3_bucket = 'brandbuddee-production'
   end
 end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, '380121918698790', '319f251fc875dde2c3f5fd44b7fbd8da'
   #provider :facebook, '380121918698790', '319f251fc875dde2c3f5fd44b7fbd8da', :scope => 'email,offline_access'#, :display => 'popup'
-  provider :twitter, 'a8uYjTwOukqYxm9S97Xk1g', 'wtn6WvMp56C6glHzXJd2aEmC2zzG50PdMJAycdt1Xs'
-  provider :facebook, '343588415732330', '8355ede143963a61b624d82ff354dc08', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.development?
-  provider :facebook, '524356277579122', '5284545fece303b49d41d1d627dcd99f', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.production?
+  provider :twitter, 'XS3ZFcP3lxmz7WFssNcF8Q', 'HHd6Pr333lDBkOcoW6FXteoBCGxUiM9gwcMl3GYccE'
+  provider :facebook, '253857021420655', 'ac29289c15bff7d445bb51e8f1646ada', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.development?
+  provider :facebook, '278238152312772', 'fbf139910f26420742f3d88f3b25f9a9', {:scope => 'email,offline_access,publish_actions,publish_stream,user_birthday,user_about_me,user_location,user_likes,user_education_history,user_website,read_friendlists,user_interests,user_hometown,user_status'} if Rails.env.production?
 end
