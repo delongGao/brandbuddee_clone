5e41525ccea51bae09a5e20b7036160875e26879
Brice Bentler <server4001@gmail.com>
2013-09-09 18:30:03 -0700

    UPDATE FB Embed Widget

--- app/assets/javascripts/invites.js
+++ app/assets/javascripts/invites.js
@@ -2,7 +2,7 @@ window.fbAsyncInit = function() {
   // init the FB JS SDK
   FB.init({
     appId      : '404234572989713', // App ID from the App Dashboard
-    channelUrl : '//www.applecrateseo.com/bb-invite-app/channel.html', // Channel File for x-domain communication
+    channelUrl : '/fbapp/channel.html', // Channel File for x-domain communication
     status     : true, // check the login status upon init?
     cookie     : true, // set sessions cookies to allow your server to access the session?
     frictionlessRequests : true, // enable frictionless requests
--- config/routes.rb
+++ config/routes.rb
@@ -237,6 +237,19 @@ Brandbuddee::Application.routes.draw do
   get "/b/:share_link" => "welcome#share"
   get "/share" => "welcome#share"
 
+  get '/fbapp/channel.html' => proc {
+  	[
+  		200,
+  		{
+  			'Pragma' => 'public',
+  			'Cache-Control' => "max-age=#{1.year.to_i}",
+  			'Expires' => 1.year.from_now.to_s(:rfc822),
+  			'Content-Type' => 'text/html'
+  		},
+  		['<script type="text/javascript" src="//connect.facebook.net/en_US/all.js"></script>']
+  	]
+  }
+
   resources :reset_passwords, except: [:show]
   
 
